697c7738942ee922317d06e20481ebd981bb0240
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2019-11-07 05:59:15 +0100

    add formatting tools

---
+++ generators/basic/prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  jsxSingleQuote: true,
+  arrowParens: "always",
+  // printWidth: 80,
+};
